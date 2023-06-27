353d2b6a3ea475353874417c6742d9f125dd2983
vigneshwaran33555 <132059498+vigneshwaran33555@users.noreply.github.com>
2023-06-27 13:55:11 +0530

    Add files via upload
    https://649a9c55d1bbbc56c160f86c--tourmaline-fairy-c7573d.netlify.app/

---
+++ project5/js.js
@@ -0,0 +1,13 @@
+function answer(){
+    let arr=document.getElementById("input").value
+let result=document.getElementById("result")
+let upper = arr.toLocaleLowerCase();
+    result.innerHTML=upper
+}
+function answer1(){
+    let arr=document.getElementById("input").value
+let result=document.getElementById("result")
+let lower= arr.toLocaleUpperCase();
+    result.innerHTML=lower
+}
+
